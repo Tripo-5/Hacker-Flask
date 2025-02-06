d6295732aa41078610a213bdf0fab5ce1f86f1e0
Tripo-5 <mcdonaldluke2@gmail.com>
2025-02-06 10:39:37 +0000

    Create models.py

---
+++ models.py
@@ -0,0 +1,15 @@
+from app import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+class Proxy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ip = db.Column(db.String(50), nullable=False)
+    port = db.Column(db.Integer, nullable=False)
+    connectivity = db.Column(db.String(10), nullable=True)
+    response_time = db.Column(db.Float, nullable=True)
+    location = db.Column(db.String(100), nullable=True)
